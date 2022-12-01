0e1f33f243fc146df1d223794b600d282e663e3c
Lucas-De-Lima <lucasramosdelima@gmail.com>
2022-12-01 20:11:56 -0300

    corrigindo erros de lint

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,31 +3,6 @@ import Header from '../components/Header';
 // import useRenderHeader from '../hooks/useRenderHeader';
 
 export default function Profile({ location }) {
-//   const [pageTitle, setPageTitle] = useState('');
-//   const { pathname } = location;
-
-  //   const { isHeader, setHeader: setIsHeader } = useRenderHeader({
-  //     renderProfileIcon,
-  //     renderSearchIcon,
-  //   });
-
-  //   console.log(isHeader.profileIcon);
-
-  //   useEffect(() => {
-  //     setIsHeader({
-  //       profileIcon: renderProfileIcon,
-  //       searchIcon: renderSearchIcon,
-  //     });
-  //   }, []);
-
-  //   useEffect(() => {
-  //     setPageTitle(() => {
-  //       const removeBar = pathname.replace('/', '');
-  //       const formatFirstLetter = removeBar.charAt(0).toUpperCase() + removeBar.slice(1);
-  //       return formatFirstLetter;
-  //     });
-  //   }, [pathname]);
-
   return (
     <Header
       renderProfileIcon
