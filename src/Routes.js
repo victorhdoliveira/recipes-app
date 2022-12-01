5c2f91627d5fa41474f51886f2283ca2df0ba08a
Victor Hugo <victorhdoliveira@gmail.com>
2022-11-30 21:22:03 -0300

    tela de perfil (req 57 - 62)

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import Recipes from './pages/Recipes';
 
 function Routes() {
@@ -14,9 +15,9 @@ function Routes() {
       {/* <Route path="/meals/:id-da-receita" component={ } />
       <Route path="/drinks/:id-da-receita" component={ } />
       <Route path="/meals/:id-da-receita/in-progress" component={ } />
-      <Route path="/drinks/:id-da-receita/in-progress" component={ } />
-      <Route path="/profile" component={ } />
-      <Route path="/done-recipes" component={ } />
+      <Route path="/drinks/:id-da-receita/in-progress" component={ } /> */}
+      <Route path="/profile" component={ Profile } />
+      {/* <Route path="/done-recipes" component={ } />
       <Route path="/favorite-recipes" component={ } /> */}
     </Switch>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,73 +1,55 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-class Login extends Component {
-  state = {
-    email: '',
-    password: '',
-    isBtnDisabled: true,
-  };
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const history = useHistory();
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value }, () => this.buttonVerify());
-  };
-
-  buttonVerify = () => {
-    const { email, password } = this.state;
+  useEffect(() => {
     const minLength = 7;
     const passwordVerify = password.length >= minLength;
     const regex = /\S+@\S+\.\S+/;
     const emailVerify = regex.test(email);
     const btnState = emailVerify && passwordVerify;
-    this.setState({ isBtnDisabled: !(btnState) });
-  };
+    setIsBtnDisabled(!btnState);
+  }, [email, password, isBtnDisabled]);
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { history } = this.props;
-    const { email } = this.state;
     const emailObject = { email };
     localStorage.setItem('user', JSON.stringify(emailObject));
     history.push('/meals');
   };
 
-  render() {
-    const { isBtnDisabled } = this.state;
-    return (
-      <form>
-        <input
-          data-testid="email-input"
-          placeholder="Digite seu e-mail"
-          type="text"
-          name="email"
-          onChange={ this.handleChange }
-        />
-        <input
-          data-testid="password-input"
-          placeholder="Senha"
-          type="password"
-          name="password"
-          onChange={ this.handleChange }
-        />
-        <button
-          className="btn-input"
-          type="submit"
-          data-testid="login-submit-btn"
-          onClick={ this.handleSubmit }
-          disabled={ isBtnDisabled }
-        >
-          Entrar
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form>
+      <input
+        data-testid="email-input"
+        placeholder="Digite seu e-mail"
+        type="text"
+        name="email"
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <input
+        data-testid="password-input"
+        placeholder="Senha"
+        type="password"
+        name="password"
+        onChange={ ({ target }) => setPassword(target.value) }
+      />
+      <button
+        className="btn-input"
+        type="submit"
+        data-testid="login-submit-btn"
+        onClick={ handleSubmit }
+        disabled={ isBtnDisabled }
+      >
+        Entrar
+      </button>
+    </form>
+  );
 }
 
-Login.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func,
-  }).isRequired,
-};
-
-export default connect()(Login);
+export default Login;
