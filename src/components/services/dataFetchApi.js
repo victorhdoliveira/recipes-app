c0897215dbaf30f033df45521b864c8012c6d8f3
Renato <renato92.hora@gmail.com>
2022-12-07 17:24:31 -0300

    Finaliza requisito 37

---
+++ src/components/services/dataFetchApi.js
@@ -0,0 +1,13 @@
+export const mealDetails = async (mealId) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+  const json = await response.json();
+  const { meals } = json;
+  return meals;
+};
+
+export const cocktailDetails = async (cocktailId) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`);
+  const json = await response.json();
+  const { drinks } = json;
+  return drinks;
+};
---
+++ src/pages/RecipeInProgress.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { cocktailDetails, mealDetails } from '../components/services/dataFetchApi';
+
+function RecipeInProgress() {
+  const { id } = useParams();
+  const history = useHistory();
+  const [recipe, setRecipe] = useState([]);
+
+  const infoItem = async () => {
+    const { pathname } = history.location;
+    let recipeInfo = recipe;
+    if (pathname.includes('/meals')) {
+      const getMeal = await mealDetails(id);
+      recipeInfo = getMeal;
+    } else if (pathname.includes('/drinks')) {
+      const getDrink = await cocktailDetails(id);
+      recipeInfo = getDrink;
+    }
+    setRecipe(recipeInfo);
+  };
+
+  useEffect(() => {
+    infoItem();
+  }, []);
+
+  const defineRecipe = () => {
+    let recipeInfo = {
+      recipeTitle: '',
+      recipeImage: '',
+      recipeIngredients: [],
+      recipeMeasures: [],
+      recipeCategory: '',
+      recipeVideo: '',
+      checkboxesObj: {},
+    };
+
+    const listIngredient = [];
+    const listMeasures = [];
+    Object.entries(recipe[0]).forEach(([key, value]) => {
+      if (key.includes('strIngredient') && value !== null && value.length > 0) {
+        listIngredient.push(value);
+      }
+
+      if (key.includes('strMeasure') && value !== null && value.length > 0) {
+        listMeasures.push(value);
+      }
+    });
+
+    if (recipe[0].strMeal) {
+      recipeInfo = {
+        recipeTitle: recipe[0].strMeal,
+        recipeImage: recipe[0].strMealThumb,
+        recipeIngredients: listIngredient,
+        recipeMeasures: listMeasures,
+        recipeCategory: recipe[0].strCategory,
+        recipeVideo: recipe[0].strYoutube,
+        recipeInstructions: recipe[0].strInstructions,
+      };
+    } else if (recipe[0].strDrink) {
+      recipeInfo = {
+        recipeTitle: recipe[0].strDrink,
+        recipeImage: recipe[0].strDrinkThumb,
+        recipeIngredients: listIngredient,
+        recipeMeasures: listMeasures,
+        recipeCategory: `${recipe[0].strAlcoholic} ${recipe[0].strCategory}`,
+        recipeVideo: recipe[0].strYoutube,
+        recipeInstructions: recipe[0].strInstructions,
+      };
+    }
+
+    return recipeInfo;
+  };
+
+  return (
+    <div>
+      {recipe.length > 0
+        && (
+          <>
+            <h1 data-testid="recipe-title">
+              {`Recipe: ${defineRecipe().recipeTitle}`}
+            </h1>
+            <img
+              data-testid="recipe-photo"
+              src={ defineRecipe().recipeImage }
+              alt={ defineRecipe().recipeTitle }
+              width="300"
+            />
+            <h2 data-testid="recipe-category">
+              { `Category: ${defineRecipe().recipeCategory}`}
+            </h2>
+
+            <p data-testid="instructions">
+              { `Instructions: ${defineRecipe().recipeInstructions}`}
+            </p>
+            <iframe
+              data-testid="video"
+              src={ defineRecipe().recipeVideo }
+              title={ defineRecipe().recipeTitle }
+            />
+
+            <div id="inProgress">
+              <button
+                data-testid="finish-recipe-btn"
+                type="button"
+              >
+                Terminar Receita
+              </button>
+              <button
+                data-testid="share-btn"
+                type="button"
+              >
+                Compartilhar
+              </button>
+
+              <button
+                data-testid="favorite-btn"
+                type="button"
+              >
+                Favoritar
+              </button>
+            </div>
+          </>
+        )}
+    </div>
+  );
+}
+
+export default RecipeInProgress;
