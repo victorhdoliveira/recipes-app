3c2d24293b9e600b6e28501be71b9aa85ee2a6ae
Victor Hugo <victorhdoliveira@gmail.com>
2023-01-23 19:25:11 -0300

    deploy3

--- src/index.js
+++ src/index.js
@@ -7,11 +7,6 @@ import './index.css';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
-app.use(express.static(__dirname));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
